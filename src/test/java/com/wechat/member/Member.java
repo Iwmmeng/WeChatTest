07561fc0eb76868412927bc2bdc5e9dd5e9b3e81
Iwmmeng <834638005@qq.com>
2018-10-17 15:35:40 +0800

    增加人员创建接口

---
+++ src/test/java/com/wechat/member/Member.java
@@ -0,0 +1,151 @@
+package com.wechat.member;
+
+import java.util.List;
+
+public class Member {
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public List<Integer> getOrder() {
+        return order;
+    }
+
+    public void setOrder(List<Integer> order) {
+        this.order = order;
+    }
+
+    public List<Integer> getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(List<Integer> department) {
+        this.department = department;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public boolean isIsleader() {
+        return isleader;
+    }
+
+    public void setIsleader(boolean isleader) {
+        this.isleader = isleader;
+    }
+
+
+
+    public boolean isTo_invite() {
+        return to_invite;
+    }
+
+    public void setTo_invite(boolean to_invite) {
+        this.to_invite = to_invite;
+    }
+
+    public String getExternal_profile() {
+        return external_profile;
+    }
+
+    public void setExternal_profile(String external_profile) {
+        this.external_profile = external_profile;
+    }
+
+    public String getExternal_position() {
+        return external_position;
+    }
+
+    public void setExternal_position(String external_position) {
+        this.external_position = external_position;
+    }
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getExpect() {
+        return expect;
+    }
+    public void setExpect(String expect) {
+        this.expect = expect;
+    }
+
+    private String userid;
+    private String name;
+    private List<Integer> department;
+    private  String alias;
+    private String mobile;
+    private List<Integer> order;
+    String position;
+    //todo 性别就为1或者2的应该怎么设置呢？
+    private int gender;
+    private String email;
+    private String telephone;
+    private  boolean isleader;
+    private  boolean enable;
+    private  boolean to_invite;
+    private String external_profile;
+    private String external_position;
+    private String expect;
+}
+
---
+++ src/test/java/com/wechat/member/MemberCreation.java
@@ -0,0 +1,90 @@
+package com.wechat.member;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import io.restassured.response.Response;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static io.restassured.RestAssured.*;
+import    static    io.restassured.matcher.RestAssuredMatchers.*;
+import     static   org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(Parameterized.class)
+public class MemberCreation {
+    @Parameterized.Parameters
+    public static List<Member> dataCSV() throws IOException {
+        return readFromCSV();
+    }
+    public static List<Member> readFromCSV() throws IOException {
+        ArrayList<Member> data=new ArrayList<Member>();
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(Member.class);
+        //mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        // TODO: 2018/10/14 以resources为根路径
+        File csvFile = new File(MemberCreation.class.getResource("/memberCreate.csv").getFile()); // or from String, URL etc
+        MappingIterator<Member> it = mapper.readerFor(Member.class).with(schema).readValues(csvFile);
+        while (it.hasNext()) {
+            Member row = it.next();
+            data.add(row);
+        }
+        return data;
+    }
+
+    @Parameterized.Parameter
+    public Member data;
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Test
+    public void testCreateMember(){
+        useRelaxedHTTPSValidation();
+        String corpid = "ww516aa64d44600560";
+        String secret ="xFjsMXKRQzA5l97r41UgZ5m-a5AdDOPwxp4HDwz1238";
+        String ACCESS_TOKEN = given().queryParams(corpid,"ww516aa64d44600560")
+                .queryParams(secret,"xFjsMXKRQzA5l97r41UgZ5m-a5AdDOPwxp4HDwz1238")
+                .get("https://qyapi.weixin.qq.com/cgi-bin/gettoken")
+                .then()
+                .statusCode(200)
+                .extract().body().jsonPath().get("access_token")
+                ;
+
+        Map map = new HashMap();
+        map.put("userid",data.getUserid());
+        map.put("name",data.getName());
+        map.put("department",data.getDepartment());
+        map.put("alias",data.getAlias());
+        map.put("mobile",data.getMobile());
+        map.put("order",data.getOrder());
+        map.put("position",data.getPosition());
+        map.put("gender",data.getGender());
+        map.put("email",data.getEmail());
+        map.put("telephone",data.getTelephone());
+        map.put("isleader",data.isIsleader());
+        map.put("enable",data.isEnable());
+        map.put("to_invite",data.isTo_invite());
+        map.put("external_profile",data.getExternal_profile());
+        map.put("external_position",data.getExternal_position());
+
+
+        String errocode = given().queryParams(map)
+                .log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token="+ACCESS_TOKEN)
+                .then()
+                .extract().body().jsonPath().get("errocode")
+                ;
+        collector.checkThat(errocode,equalTo(data.getExpect()));
+
+    }
+
+}
